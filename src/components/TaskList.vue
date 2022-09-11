4ee3d62b8b7c073daa371ac8f52cb0cc78489be1
mohaimenjatri <mohaimen.jatri@gmail.com>
2022-09-11 15:00:16 +0600

    task list moves to task componet

---
+++ src/components/TaskList.vue
@@ -0,0 +1,28 @@
+<template>
+  <table>
+    <thead>
+    <th>SI</th>
+    <th>Task</th>
+    <th>Status</th>
+    <th>Action</th>
+    </thead>
+    <tbody>
+    <tr v-for="(task, index) in tasks">
+      <td>{{index+1}}</td>
+      <td>{{task.task}}</td>
+      <td>{{task.status}}</td>
+    </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+  export default {
+    props: {
+      tasks: Object
+    },
+    setup() {
+
+    }
+  }
+</script>
--- src/modules/products/views/taskListPage.vue
+++ src/modules/products/views/taskListPage.vue
@@ -1,28 +1,15 @@
 <template>
   <div>Task List</div>
-
-  <table>
-    <thead>
-      <th>SI</th>
-      <th>Task</th>
-      <th>Status</th>
-      <th>Action</th>
-    </thead>
-    <tbody>
-      <tr v-for="(task, index) in tasks">
-        <td>{{index+1}}</td>
-        <td>{{task.task}}</td>
-        <td>{{task.status}}</td>
-      </tr>
-    </tbody>
-  </table>
+  <TaskList :tasks="tasks" />
 </template>
 
 <script>
 import store from "@/store";
 import {computed} from 'vue'
+import TaskList from "@/components/TaskList";
 
 export default {
+  components: {TaskList},
   setup() {
     const getTaskList = () =>{
       store.dispatch("products/getTaskList")
