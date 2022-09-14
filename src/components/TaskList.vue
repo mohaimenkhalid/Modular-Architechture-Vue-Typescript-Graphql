71c3700db93d8dfacacdc24a5732e16867dca606
mohaimenjatri <mohaimen.jatri@gmail.com>
2022-09-14 17:47:33 +0600

    tasklist component moves to ts

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -8,21 +8,23 @@
     </thead>
     <tbody>
     <tr v-for="(task, index) in tasks">
-      <td>{{index+1}}</td>
-      <td>{{task.task}}</td>
-      <td>{{task.status}}</td>
+      <td>{{ index + 1 }}</td>
+      <td>{{ task.task }}</td>
+      <td>{{ task.status }}</td>
     </tr>
     </tbody>
   </table>
 </template>
 
-<script>
-  export default {
-    props: {
-      tasks: Object
-    },
-    setup() {
+<script lang="ts">
+import {defineComponent} from "vue";
 
-    }
+export default defineComponent({
+  props: {
+    tasks: Object
+  },
+  setup() {
+
   }
+})
 </script>
